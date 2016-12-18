df53c9c3444e239ca1d73d564909393f69d047c9
Alex Lopatin <lopatin.rnd@gmail.com>
2016-12-18 03:02:10 +0300

    expectation_matchers compleet, before, let

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,44 +1,51 @@
 require 'car'
 
+describe 'Car' do
 describe 'attributes' do
+
+  let( :car ) { Car.new }
+
+  #'let' is better than 'before' to
+  #setting up instance variables
+  #before(:example) do
+  #  @car = Car.new
+  #end
+
   it "allows reading and writing to :make" do
-    car = Car.new
     car.make = 'Test'
     expect(car.make).to eq('Test')
   end
   it "allows reading and writing to :year" do
-    car = Car.new
     car.year = 9999
     expect(car.year).to eq(9999)
   end
   it "allows reading and writing to :color" do
-    car = Car.new
     car.color = 'Foo'
     expect(car.color).to eq('Foo')
   end
   it "allows reading :weels" do
-    car = Car.new
     expect(car.wheels).to eq(4)
   end
 end
 
 describe '.colors' do
+  let(:colors) { ['blue', 'black', 'red', 'green'] }
   it "returns an array of colors" do
-    c = ['blue', 'black', 'red', 'green']    
-    expect(Car.colors).to match_array(c)
+    expect(Car.colors).to match_array(colors)
   end
 end
 
 describe '#full_name' do
+  let(:honda) { Car.new(:make => 'Honda', :year => 2004, :color => 'blue') }
+  let(:new_car) { Car.new }
+
   it "returns a string of expected format" do
-    @honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
-    expect(@honda.full_name).to eq('2004 Honda (blue)')
+    expect(honda.full_name).to eq('2004 Honda (blue)')
   end
   context "when initialized with no arguments" do
     it "returns string using defaults arguments" do
-      car = Car.new
-      expect(car.full_name).to eq('2007 Volvo (unknown)')
+      expect(new_car.full_name).to eq('2007 Volvo (unknown)')
     end
   end
 end
-
+end
--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -253,16 +253,41 @@ end
     it 'will match by sending matchers as arguments to matchers' do
       string = "hello"
       expect { string = "goodby" }.to change { string }.from( match(/ll/)).to( match(/oo/) )
+
+      hash = {:a => 1, :b => 2, :c => 3 }
+      expect(hash).to include(:a => be_odd, :b => be_even, :c => be_odd)
+      expect(hash).to include(:a => be > 0, :b => be_within(2).of(4))
     end
+    it 'will match using noun-phrase aliases for matchers' do
+      #these are built-in aliases that make 
+      #specs reads better by using noun-based
+      #phrases instead a verb-based phrases
+      fruits = ['apple', 'banana', 'cherry']
+      expect(fruits).to start_with( start_with('a') ) &
+        include( match(/a.a.a/) ) &
+        end_with( end_with('y') )
+      
+      #improved version of the previouse example 
+      #'start_with' becomes 'a_string_starting_with'
+      #'end_with' becomes 'a_string_ending_with'
+      #'match' becomes 'a_string_matching
+      fruits = ['apple', 'banana', 'cherry']
+      expect(fruits).to start_with( a_string_starting_with('a') ) &
+        include( a_string_matching(/a.a.a/) ) &
+        end_with( a_string_ending_with('y') )
+
+      #valid but awkward example
+      array = [1,2,3]
+      expect(array).to start_with( be <= 2 ) |
+        end_with( be_within(1).of(5) )
+
+      #improved versionof the previouse example
+      #'be <= 2' becomes 'a_value <= 2'
+      #'be_within' becomes 'a_value_within'
+      array = [1,2,3]
+      expect(array).to start_with( a_value <=2 ) |
+        end_with( a_value_within(1).of(5) )
+    end
+
   end
-
-
-
-
-
-
-
-
-
-  
 end
